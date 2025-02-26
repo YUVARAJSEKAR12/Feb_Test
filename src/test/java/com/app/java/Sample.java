0155250a190bf1667813342554637ea877307cc9
YUVARAJSEKAR12 <61101411+YUVARAJSEKAR12@users.noreply.github.com>
2025-02-26 06:28:19 +0530

    Update Sample.java

--- src/test/java/com/app/java/Sample.java
+++ src/test/java/com/app/java/Sample.java
@@ -6,7 +6,7 @@ public class Sample {
 	
 	public static void main(String[] args) {
 		int a []= {1,1,2,2};
-		
+		//String one
 	//	Integer[] array = Arrays.stream(a).distinct().toArray(Integer[]:: new);
 	//	System.out.println(Arrays.toString(array));
 		int b []= new int [a.length];
